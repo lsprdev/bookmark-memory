2037dcd7b628a1b66f5b97ef15a5f439c7117b82
OGabrielPereira <ogabrielpereira@pm.me>
2022-07-16 23:52:22 -0300

    front-end added

---
+++ front/src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+      // component: () => import('../views/AboutView.vue')
+
+  ]
+})
+
+export default router
