0bf022dd04ba1a537a1e98d69bea3380d70665e5
OGabrielPereira <ogabrielpereira@pm.me>
2022-07-17 01:34:59 -0300

    Navbar ajusts

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,23 +1,23 @@
 <template>
     <span>
-        <nav class="bg-[#000000] shadow-xl">
-            <div class="max-w-6xl mx-auto px-4">
+        <nav>
+            <div class="max-w-7xl mx-auto">
 
                 <div class="flex justify-between">
 
                     <div>
-                        <router-link to="/" class="flex items-center py-6 px-2">
-                            <span class="text-white font-semibold text-xl tracking-tight px-2 font">BookMark Memory</span>
+                        <router-link to="/" class="flex items-center py-3 px-2">
+                            <span class="text-black font-semibold text-xl tracking-tight font">BookMark Memory</span>
                         </router-link>
                     </div>
 
                     <div class="flex items-center py-4 px-2">
                         
-                        <router-link class="py-2 px-6 font-semibold text-white rounded hover:bg-[#ffffff11] transition duration-300 mx-1" to="">
+                        <router-link class="px-6 font-semibold text-black rounded hover:bg-blue-200 transition duration-300 mx-1" to="">
                             <span>Login</span>
                         </router-link>
 
-                        <router-link class="py-2 px-6 font-semibold text-white rounded hover:bg-[#ffffff11] transition duration-300 mx-1" to="">
+                        <router-link class="px-6 font-semibold text-black rounded hover:bg-blue-200 transition duration-300 mx-1" to="">
                             <span>Sign up</span>
                         </router-link>
 
